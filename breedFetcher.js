ab803ec5151c6d848e93562f0be95057ac480945
SFxImpulse <braverman.david.business@gmail.com>
2022-07-07 19:06:14 +0000

    added a cat api breed info retriever

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+const args = process.argv.slice(2);
+const request = require("request");
+
+const retrieveBreedDescription = () => {
+  request("https://api.thecatapi.com/v1/breeds/", (err, response, body) => { 
+    if (err) {
+      console.log("There was an error:");
+    }
+    let breedChecker = false;
+    const bodyObj = JSON.parse(body);
+    for (let breed of bodyObj) {
+      if (args[0] === breed.name) {
+        console.log(breed.name);
+        console.log(breed.description);
+        breedChecker = true;
+      }
+    }
+    if (breedChecker === false) {
+      console.log("That's not a breed");
+    }
+  });
+};
+
+retrieveBreedDescription(args);
