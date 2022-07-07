c874f860dfa1ce8b3c1d02e52b37d6072d56fc96
SFxImpulse <braverman.david.business@gmail.com>
2022-07-07 19:48:25 +0000

    refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,6 @@
-const args = process.argv.slice(2);
 const request = require("request");
 
-const retrieveBreedDescription = () => {
+const retrieveBreedDescription = (breedName, callback) => {
   request("https://api.thecatapi.com/v1/breeds/", (err, response, body) => {
     if (err) {
       console.log("There was an error:");
@@ -9,16 +8,15 @@ const retrieveBreedDescription = () => {
     let breedChecker = false;
     const bodyObj = JSON.parse(body);
     for (let breed of bodyObj) {
-      if (args[0] === breed.name) {
-        console.log(breed.name);
-        console.log(breed.description);
+      if (breedName === breed.name) {
         breedChecker = true;
+        callback(null, breed.description);
       }
     }
     if (breedChecker === false) {
-      console.log("That's not a breed");
+      callback("That's not a breed...");
     }
   });
 };
 
-retrieveBreedDescription(args);
+module.exports = { retrieveBreedDescription };
